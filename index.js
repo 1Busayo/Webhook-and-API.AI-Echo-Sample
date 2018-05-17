9f9999e467b9ef553835b58816d567c02a731b41
1Busayo <35940016+1Busayo@users.noreply.github.com>
2018-05-17 23:07:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -33,14 +33,12 @@ restService.post("/echo", function(req, res) {
   var speech =
     req.body.result &&
     req.body.result.parameters &&
-    req.body.result.parameters.echoText
+    req.body.queryResult.parameters.echoText
       ? req.body.result.parameters.echoText
       : "Seems like some problem. Speak again.";
   return res.json({
 
-    "fulfillmentText": speech,
-   
-   "webhookSource":"webhook-sample-source" 
+    "fulfillmentText": speech
   });
 });
 
